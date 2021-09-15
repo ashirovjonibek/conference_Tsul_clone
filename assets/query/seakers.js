aee9642d2218b41b887c0c4c3b8e05867e2a8a13
webdeveloper2023 <nazirovrafiqjon95@gmail.com>
2021-09-15 17:57:51 +0500

    Select and langs are added
    Select and langs are added

--- assets/query/seakers.js
+++ assets/query/seakers.js
@@ -437,18 +437,18 @@ let dataStatic=[
     {
         "id": "homeNav",
         "uz":"Bosh sahifa",
-        "ru": "Дом",
+        "ru": "Главная страница",
         "en": "Home"
     },
     {
         "id": "aboutNav",
-        "uz":"Haqida",
+        "uz":"Biz haqimizda",
         "ru": "O нас",
         "en": "About"
     },
     {
         "id": "speakerNav",
-        "uz":"Spekerlar",
+        "uz":"Nutq so'zlovchilar",
         "ru": "Оратор",
         "en": "Speakers"
     },
@@ -460,8 +460,8 @@ let dataStatic=[
     },
     {
         "id": "venueNav",
-        "uz":"Joylar",
-        "ru": "Место проведения",
+        "uz":"Manzil",
+        "ru": "Адрес",
         "en": "Venue"
     },
     {
@@ -471,11 +471,59 @@ let dataStatic=[
         "en": "Gallery"
     },
     {
-        "id": "homeText",
-        "uz":"Bosh sahifa",
-        "ru": "Дом",
-        "en": "Home"
-    }
+        "id": "contactNav",
+        "uz":"Bog'lanish",
+        "ru": "Kонтакт",
+        "en": "Contact"
+    },
+    {
+        "id": "holidayBigtext",
+        "uz":"Toshkent davlat yuridik universiteti ",
+        "ru": "30 лет Ташкентский государственный юридический университет",
+        "en": "Tashkent State University of Law is 30 years old"
+    },  
+    {
+        "id": "holidayDate",
+        "uz":"16-17 Sentyabr, Hyatt mehmonhonasi, Toshkent",
+        "ru": "16-17 сентября, Гостиница Hyatt, Ташкент",
+        "en": "16-17 September, Hyatt Regency, Tashkent"
+    },   
+    {
+        "id": "aboutTheEvent",
+        "uz":"Marosim haqida",
+        "ru": "О церемонии",
+        "en": "About The Event"
+    },  
+    {
+        "id": "aboutTheEvent2",
+        "uz":"Marosim haqida",
+        "ru": "О церемонии",
+        "en": "About The Event"
+    }, 
+    {
+        "id": "eventInfo",
+        "uz":"Toshkent Davlat Yuridik Universitetining 30 yoshiga bag'ishlangan marosim.",
+        "ru": "Церемония, посвященная 30-летию Ташкентского государственного юридического университета.",
+        "en": "Ceremony dedicated to the 30th anniversary of Tashkent State Law University."
+    },
+    {
+        "id": "where",
+        "uz":"Marosim o'tkaziladigan joy",
+        "ru": "Место проведения церемонии",
+        "en": "where"
+    },
+    {
+        "id": "1",
+        "uz":"Hyatt mehmonhonasi, Toshkent",
+        "ru": "Гостиница Hyatt, Ташкент",
+        "en": "Hyatt Regency, Tashkent"
+    }, 
+    {
+        "id": "2",
+        "uz":"Hyatt mehmonhonasi, Toshkent",
+        "ru": "Гостиница Hyatt, Ташкент",
+        "en": "Hyatt Regency, Tashkent"
+    }, 
 ];
 
 function speakerList(lang) {
